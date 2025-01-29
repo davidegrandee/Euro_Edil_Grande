45167e965d1b034d9a6ba681e747f036819aa6ff
Museo Virtuale <grandedavide6@gmail.com>
2025-01-29 18:37:19 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+// Funzione per mostrare/nascondere la sezione "Chi Siamo"
+function toggleAbout() {
+    let aboutSection = document.getElementById("about-section");
+    let arrow = document.getElementById("arrow");
+
+    // Alterna la visibilità
+    if (aboutSection.style.display === "block") {
+        aboutSection.style.display = "none";
+        arrow.innerHTML = "▼"; // Mostra freccia in giù
+    } else {
+        aboutSection.style.display = "block";
+        arrow.innerHTML = "▲"; // Mostra freccia in su
+    }
+}
